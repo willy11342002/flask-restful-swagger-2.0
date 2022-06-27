ce67ae4f6dab1301053f758fb9a589daa9513f09
METAEDGE-TW\William.Liu <William.Liu@MetaEdge.com.tw>
2022-06-27 12:05:12 +0800

    include pattern

--- flask_restful_swagger_2/__init__.py
+++ flask_restful_swagger_2/__init__.py
@@ -309,6 +309,8 @@ class _RequestParserExtractorImpl(_BaseExtractorImpl):
             cls.__update_reqparser_arg_as_array(arg, param)
         else:
             param['type'] = cls._get_swagger_arg_type(arg.type)
+        if arg.pattern:
+            param['pattern'] = arg.pattern
         if arg.format == date:
             param['format'] = 'date'
         if arg.format == datetime:
